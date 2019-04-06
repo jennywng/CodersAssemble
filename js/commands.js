8fa3b1cd9723d07a04dd6ee4c6bfd64582a3c0f4
Jenny <jrw3mx@virginia.edu>
2019-04-06 16:58:33 -0400

    mov

--- js/commands.js
+++ js/commands.js
@@ -62,26 +62,18 @@ $("#inc-btn").click(function(){
     $("#" + add1 + "-val").fadeIn("slow");
 });
 
+$("#mov-btn").click(function(){
+    var add1 = $('#mov-input1').val();
+    var add2 = $('#mov-input2').val();
+    console.log(add1);
+    console.log(add2);
 
-// function push() {
-//     var value = document.getElementById("pushvalue").value;
-//     document.getElementById("stack1").value = document.getElementById(value).value;
-// }
-// </script>
-// <a class="btn">Pop</a>  <input type="text" placeholder="Register (i.e. RAX)"><br>
-// <a class="btn" onclick="mov();">Mov</a>  <input type="text" id="movvalue1" placeholder="To register (i.e. RAX)"> <input type="text" id="movvalue2" placeholder="From register (i.e. RBX)"><br>
-// <script>
-// function mov() {
-//     var value2 = document.getElementById("movvalue2").value;
-//     var value1 = document.getElementById("movvalue1").value;
-//     document.getElementById(value1).value = document.getElementById(value2).value;
-// }
-// </script>
-// <a class="btn">Add</a>  <input type="text" placeholder="To register (i.e. RAX)"> <input type="text" placeholder="From register (i.e. RBX)"><br>
-// <a class="btn">Sub</a>  <input type="text" placeholder="To register (i.e. RAX)"> <input type="text" placeholder="From register (i.e. RBX)"><br>
-// <a class="btn" onclick="inc();">Inc</a>  <input type="text" id="incvalue" placeholder="Register (i.e. RAX)"><br>
-// <script>
-// function inc() {
-//     var txt = document.getElementById("incvalue").value;
-//     document.getElementById(txt).value++;
-// }
+    var val1 = parseInt($("#" + add1 + "-val").text());
+    var val2 = parseInt($("#" + add2 + "-val").text());
+    console.log(val1);
+    console.log(val2);
+
+    $("#" + add1 + "-val").hide();
+    $("#" + add1 + "-val").text(val2);
+    $("#" + add1 + "-val").fadeIn("slow");
+});
