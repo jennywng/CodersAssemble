b55b3cea83af209142ec08fb97bbf1a2149369a7
Jenny <jrw3mx@virginia.edu>
2019-04-06 16:21:26 -0400

    commands

--- js/commands.js
+++ js/commands.js
@@ -1,10 +1,23 @@
 
 
 $("#add-btn").click(function(){
-    var $r9_val = $('#r9-val').value;
-    var $r8_val = $('#r8-val').value;
-    var sum = $r9_val + $r8_val;
+    var $r9_val = parseInt($('#r9-val').text);
+    console.log(typeof($r9_val));
+    var $r8_val = parseInt($('#r8-val').text);
+    console.log(typeof($r8_val));
+    var sum = 900;
     $('#r8-val').hide();
     $('#r8-val').text(sum);
     $('#r8-val').fadeIn("slow");
+});
+
+$("#sub-btn").click(function(){
+    var $r9_val = parseInt($('#r9-val').text);
+    console.log(typeof($r9_val));
+    var $r8_val = parseInt($('#r8-val').text);
+    console.log(typeof($r8_val));
+    var sum = 100;
+    $('#r9-val').hide();
+    $('#r9-val').text(sum);
+    $('#r9-val').fadeIn("slow");
 });
